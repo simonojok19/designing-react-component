41dd4dbe7b440b9cf6a11c5caa2afd1d94bed601
simonojok19 <simonojok19@gmail.com>
2021-06-11 21:39:36 +0300

    created a new components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 import {data} from "../SpeakerData";
 
+function Session({title, room}) {
+    return (
+        <span className="session w-100">
+            {title} <strong>Room: {room.name}</strong>
+        </span>
+    );
+}
+
 const IndexPage = () => {
 
     return (
@@ -28,9 +36,7 @@ const IndexPage = () => {
                                     </div>
                                 </div>
                                 <div className="sessionBox card h-250">
-                            <span className="session w-100">
-                                {sessions[0].title} <strong>Room: {sessions[0].room.name}</strong>
-                            </span>
+                                    <Session title={sessions[0].title} room={sessions[0].room.name}/>
                                 </div>
                             </div>
                         </div>
