f50dc31bbc9934dd778e56fc8c436cce389fd1c8
simonojok19 <simonojok19@gmail.com>
2021-06-11 22:06:08 +0300

    cleaned the and finished speaker demographics

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -26,7 +26,7 @@ function SpeakerImage({id, first, last}) {
     );
 }
 
-function SpeakerDemographics({first, last, bio, company, favorite}) {
+function SpeakerDemographics({first, last, bio, company, favorite, twitterHandle}) {
     return (
         <div className="speaker-info">
             <div className="d-flex justify-content-between mb-3">
@@ -35,7 +35,17 @@ function SpeakerDemographics({first, last, bio, company, favorite}) {
                 </h3>
             </div>
             <div>
-                <p>{bio} {company} {favorite}</p>
+                <p className="card-description">{bio}</p>
+                <div className="social d-flex flex-row mt-4">
+                    <div className="company">
+                        <h5>Company</h5>
+                        <h6>{company}</h6>
+                    </div>
+                    <div className="twitter">
+                        <h5>Twitter</h5>
+                        <h6>{twitterHandle}</h6>
+                    </div>
+                </div>
             </div>
         </div>
     );
